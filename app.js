f7ac8fb18ec882ffb59b21ee8137b1758bf10edf
Luan <luanrdag@gmail.com>
2025-02-08 16:27:01 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,34 @@
+/*
+let diaDaSemana = prompt("Qual é o dia da semana");
+
+if(diaDaSemana == "Domingo" || diaDaSemana == "Sábado") {
+    alert("Bom final de semana");
+} else {
+    alert("Boa semana");
+}
+
+
+let numero = prompt("Digite um número");
+
+if(numero > 0) {
+    alert("Positivo");
+} else if(numero < 0) {
+    alert("Negativo");
+} else {
+    alert("Zero");
+}
+    */
+
+let pontos = 0;
+let tentativas = 0;
+
+while(pontos < 100) {
+    tentativas++;
+    pontos = prompt("Digite a pontuação");
+    if(tentativas > 0 && pontos < 100) {
+        alert("Tente novamente");
+    } 
+}
+
+alert("Parabéns, você tirou mais 100 pontos");
+alert(`Foram ${tentativas} tentativas`);
